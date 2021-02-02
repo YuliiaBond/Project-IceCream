e69f4c886c4e81cbc0c22b85846af31c159bc446
Yuliia Bondarenko <bondarenko047@gmail.com>
2021-02-02 13:09:56 +0200

    Updates

--- slider.d16eec5e.js
+++ slider.d16eec5e.js
@@ -172,7 +172,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "62743" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "53880" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
